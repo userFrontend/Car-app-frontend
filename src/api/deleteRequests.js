a511becb81679f92f75378c5671be9c38b3e155e
userFrontend <aba06096@gmail.com>
2024-08-18 11:13:17 +0500

    change deleteRequest

--- src/api/deleteRequests.js
+++ src/api/deleteRequests.js
@@ -4,9 +4,9 @@ const serverUrl = process.env.REACT_APP_SERVER_URL;
 
 const API = axios.create({baseURL: serverUrl});
 
-export const delProd = (id, {method}) => {
+export const delProd = (id, method) => {
     const token = localStorage.getItem("access_token");
     return API.delete(`/api/${method}/${id}`, {headers: {token}})}
-export const deleteUser = (id, data, {method}) => {
+export const deleteUser = (id, data, method) => {
     const token = localStorage.getItem("access_token");
     return API.post(`/api/${method}/${id}`, data, {headers: {token}})}
