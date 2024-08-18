7c00313d3cd13e60025f656796d01efcbd428081
userFrontend <aba06096@gmail.com>
2024-08-18 11:13:39 +0500

    change updateRequest

--- src/api/updateRequests.js
+++ src/api/updateRequests.js
@@ -4,7 +4,7 @@ const serverUrl = process.env.REACT_APP_SERVER_URL;
 
 const API = axios.create({baseURL: serverUrl});
 
-export const updateProd = (id, data, {method}) => {
+export const updateProd = (id, data, method) => {
     const token = localStorage.getItem("access_token")
     return API.put(`/api/${method}/${id}`, data, {headers: {token}})
 }; 
