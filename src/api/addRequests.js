426e08a7acc8d1427270a242954649bfc80905ff
userFrontend <aba06096@gmail.com>
2024-08-18 11:13:28 +0500

    change addRequest

--- src/api/addRequests.js
+++ src/api/addRequests.js
@@ -4,9 +4,9 @@ const serverUrl = process.env.REACT_APP_SERVER_URL;
 
 const API = axios.create({baseURL: serverUrl});
 
-export const addProd = (data, {method}) => {
+export const addProd = (data, method) => {
     const token = localStorage.getItem("access_token")
     return API.post(`/api/${method}`, data, {headers: {token}})}; 
-export const addComment = (id, data, {method}) => {
+export const addComment = (id, data, method) => {
     const token = localStorage.getItem("access_token")
     return API.post(`/api/${method}/${id}`, data, {headers: {token}})}; 
