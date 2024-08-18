ded35bc049beb57caa121bfa03f31944bb941b49
userFrontend <aba06096@gmail.com>
2024-08-18 11:13:50 +0500

    change messageRequest

--- src/api/messageRequests.js
+++ src/api/messageRequests.js
@@ -4,6 +4,6 @@ const serverUrl = process.env.REACT_APP_SERVER_URL;
 
 const API = axios.create({baseURL: serverUrl});
 
-export const messageData = (id, {method}) => {
+export const messageData = (id, method) => {
     const token = localStorage.getItem("access_token")
     return API.get(`/api/${method}/${id}`, {headers: {token}})}; 
